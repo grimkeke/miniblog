43257d11d8e1878ba52fb559a7f253c4b930f80c
grimkeke <grimkeke@gmail.com>
2013-03-15 01:03:39 +0800

    add models

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
-from os import urandom
+import os
 
 CSRF_ENABLED = True
-SECURE_KEY = urandom(24)
+SECRET_KEY = os.urandom(24)
+OPENID_PROVIDERS = [
+        { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+        { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+        { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+        { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+        { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
