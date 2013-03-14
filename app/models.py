1f869dae285f0edfa18f6caf14c87c2fea129a98
grimkeke <grimkeke@gmail.com>
2013-03-15 01:18:45 +0800

    add db_migrate

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     nickname = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
-    role = db.Colunm(db.SmallInteger default = ROLE_USER)
+    role = db.Column(db.SmallInteger, default = ROLE_USER)
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
