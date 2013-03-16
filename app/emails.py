db16a87a0ecba3d878c091a5b60699adbe858c78
grimkeke <grimkeke@gmail.com>
2013-03-17 02:08:05 +0800

    refine email bug

--- app/emails.py
+++ app/emails.py
@@ -1,14 +1,16 @@
 from flask.ext.mail import Message
 from flask import render_template
-from app import mail
+from app import mail, app
 from config import ADMINS
+import threading
 from threading import Thread
 
 def send_async_email(msg):
+    app.logger.debug('prepare sending email')
     mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
-    msg = Message(subject, sender, recipients)
+    msg = Message(subject, sender = sender, recipients = recipients)
     msg.body = text_body
     msg.html = html_body
     thr = threading.Thread(target = send_async_email, args = [msg])
